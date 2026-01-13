91ff9c841103f7c8465304d4f9c679f27339d3e8
dheim123 <dheim@midland.k12.ia.us>
2026-01-13 18:13:41 +0000

    1

--- Precision.cs
+++ Precision.cs
@@ -1,6 +1,8 @@
 ﻿namespace knightmoves;
 public class Precision
 {
-    // Add your code here
+    public int MaxInteger = 2147483647; 
+    public double DecimalProduct = 2.53468d * 5.75956733d;
+
     
 }
